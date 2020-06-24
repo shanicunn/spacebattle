f2025808958750f9773d6c450b5f04d62d9c1097
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-06-24 14:41:04 -0400

    test commit

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+// 6 Alien ships: You fight 1 ship at a time
+// UssSchwar attacks first and in order
+// Destroy 1 ship then attack next or retreat
+// Retreat ends game, win if 6 destroyed, lose if you are destroyed
+// Hull < 0 = destroyed
+// Firepower = power of hit
+// Accuracy = Math.random(0,1) chance between 0-1 that you will hit
+// Alien accuracy = .7
+// UssSchwar accuracy = .6
+
+
+// Pop-ups for input via prompt("")
+// Pop-ups with information via alert("")
+
+// Game play loop
+while (Actor.hull > 0) {
+
+}
+
+// Actors
+class Actor {
+    constructor(hull, fire, accu) {
+        this.hull;
+        this.fire;
+        this.accu;
+
+    }
+}
+
+let ussSchwar = new Actor("USS Schwarzenneger", num, num);
+let alien1 = new Actor ("Alien Ship 1", num , num )
+let alien2 = new Actor()
+let alien3 = new Actor()
+let alien4 = new Actor()
+let alien5 = new Actor()
+let alien6 = new Actor()
+
+// Actions
+Actor.attack = function () {
+    // loop over all 6 alien ships
+};
+
+accuracy = () => {
+    if ()
+}
